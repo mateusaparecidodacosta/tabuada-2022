2a2e5d40c0b53dd2909e625e2ee78cd02c9db8aa
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-20 20:18:20 -0300

    finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double numero , contador , produto;
+
+Console.Write ("Tabuada do Número: ");
+numero = Convert.ToDouble(Console.ReadLine()!);
+
+if (numero < 0 || numero > 10 )
+{
+    Console.WriteLine("Digite um Número de 0 até o 10 ");
+}
+else
+{
+    contador = 0; 
+
+    while (contador <= 10)
+    {
+        produto = numero * contador;
+        Console.WriteLine($"{numero} x {contador++} = {produto}");
+    }
+}
